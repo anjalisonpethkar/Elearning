81b011bcf8d57fbe309538cdae309aa3b119eb0f
asonpeth <asonpeth@NBSP05813W-TP.yellowdc.com>
2021-04-21 01:22:41 +0530

    Upskill Elearning

---
+++ Elearning/src/test/java/StepDefinition/Steps.java
@@ -0,0 +1,71 @@
+package StepDefinition;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Steps {
+	WebDriver driver;
+	@Given("I am able to naviagte onto the login page")
+	public void i_am_able_to_naviagte_onto_the_login_page() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Desktop_Backup\\SeleniumStrechedProject\\AllProjectJars\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "C:\\Desktop_Backup\\SeleniumStrechedProject\\AllProjectJars\\chromedriver.exe\");
+		   //this is to open the new instance of browser
+		   driver = new ChromeDriver();
+		   //This is basically to opent the application
+		   driver.get("http://elearningm1.upskills.in/");
+		   //if i want to maximize the screen
+		   driver.manage().window().maximize();
+		   //if i want to wait for sometime
+		   driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    
+	}
+	
+	@When("I Enter the username as {string}")
+	public void i_enter_the_username_as(String string) {
+		driver.findElement(By.id("login")).sendKeys(string);
+	  
+	}
+	
+	
+	@When("I Enter the Password as {string}")
+	public void i_enter_the_password_as(String string) {
+		 driver.findElement(By.id("password")).sendKeys(string);
+	    
+	}
+	
+	
+	@When("I Click on the Login button")
+	public void i_click_on_the_login_button() {
+		driver.findElement(By.id("form-login_submitAuth")).click();
+	    
+	}
+	
+	@Then("I should see the username as {string}")
+	public void i_should_see_the_username_as(String string) throws InterruptedException {
+		String name = driver.findElement(By.id("homepage-course")).getText();
+	    String Actualdata = string;
+	    Assert.assertEquals(name, Actualdata);
+	    System.out.println("login successfull");
+	    driver.close();
+	   
+	}
+	
+	@Then("I should see a Error Message as {string}")
+	public void i_should_see_a_error_message_as(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	   String actualerr = driver.findElement(By.id("spanMessage")).getText();
+	   
+	   String Expectederr = "Invalid credentials";
+	   Assert.assertEquals(Expectederr, actualerr);
+	   driver.close();
+	}
+
+}
